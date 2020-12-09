349888ca74e7fd251252da7c38a659c2e250905f
partum <jarringt@uccs.edu>
2020-12-09 15:15:26 -0700

    ???
    Well. Some of my code got deleted, but I never commited it so :/

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+numTasks = 0;
+
 function getLocation(){
     const cbImportant = document.getElementById('Important');
     const cbUrgent = document.getElementById('Urgent');
@@ -12,8 +14,8 @@ function getLocation(){
     }
 }
 
-function addTask() {
-    var inputTask = document.getElementById("task-input").elements[0].value;
+function addTask(inputTask = document.getElementById("task-input").elements[0].value, location = getLocation()) {
+    //var inputTask = document.getElementById("task-input").elements[0].value;
 
     //add task with a check box (and a br)
     var newCheck = document.createElement("input");
@@ -28,22 +30,101 @@ function addTask() {
       });
 
     var newTask = document.createElement("label");
+    newTask.setAttribute("id", index)
     var node = document.createTextNode(" " + inputTask);
     newTask.appendChild(node);
 
     var newBreak = document.createElement("br");
 
-    var section = document.getElementById(getLocation());
+    var section = document.getElementById(location);
     section.appendChild(newCheck);
     section.appendChild(newTask);
     section.append(newBreak);
 }
 
 function removeTasks(elmA, elemB, elemC) { //this could be shorter if I had added the task (box, name, br) in a div
+    localStorage.removeItem(elemB.id);
+    index = elemB.id;
+    document.getElementById("submit").disabled = false;
     elmA.remove();
     elemB.remove();
     elemC.remove();
 }
 
 
+function newIndex(){
+    if (index in localStorage){
+        index++;
+        if (index > 21){
+            index = 0;
+        }
+        if (localStorage["length"] > 20){
+            alert("That's enough tasks!");
+            index = 'exit condition'; 
+            document.getElementById("submit").disabled = true;
+            console.log(localStorage);
+        }
+        newIndex();
+    }
+}
 
+
+//setting index as 0 or as its last saved value
+var taskCount = document.getElementById("topLeft").childElementCount + document.getElementById("topRight").childElementCount 
++ document.getElementById("bottomLeft").childElementCount + document.getElementById("bottomRight").childElementCount; 
+if (taskCount == 0){
+    index = 0;
+}else{
+    index = localStorage.getItem('count');
+}
+// console.log(localStorage["length"]);
+
+
+// localStorage.removeItem(0);
+// localStorage.removeItem(1);
+// localStorage.removeItem(2);
+// localStorage.removeItem(3);
+// localStorage.removeItem(4);
+// localStorage.removeItem(5);
+// localStorage.removeItem(6);
+// localStorage.removeItem(7);
+// localStorage.removeItem(8);
+// localStorage.removeItem(9);
+// localStorage.removeItem(10);
+// localStorage.removeItem(11);
+// localStorage.removeItem(12);
+// localStorage.removeItem(13);
+// localStorage.removeItem(14);
+// localStorage.removeItem(15);
+// localStorage.removeItem(16);
+// localStorage.removeItem(17);
+// localStorage.removeItem(18);
+// localStorage.removeItem(19);
+// localStorage.removeItem(20);
+// localStorage.removeItem(21);
+
+var x = 0;
+var startUp = true;
+while (x < 21){
+    oldTask = localStorage.getItem(x);
+    if (oldTask != null){
+        oldTask = oldTask.split("$*!");
+        //localStorage.removeItem(x);
+        addTask(oldTask[0], oldTask[1]);
+        localStorage.setItem('count', index);
+    }
+    x++;
+    index = x;
+
+}
+var startUp = false;
+//setting a new index (because it is now 21)
+x = 0;
+while(x < 21){
+    if (localStorage.getItem(x) == null){
+        index = x;
+        break;
+    }
+    x++;
+}
+console.log(index);
